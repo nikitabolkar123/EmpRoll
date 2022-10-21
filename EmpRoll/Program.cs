9db49f7d2ec3e1d9932185dd23a44560138f5233
nikitabolkar123 <bolkarnikita2001@gmail.com>
2022-10-21 11:02:36 -0700

    Added usecase1

---
+++ EmpRoll/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace EmpRoll
+{
+    internal class Program 
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random(); 
+            int value = random.Next(0, 2); //it will generate random value in between 0 and 2
+            if (value == 0)
+            {
+                Console.WriteLine("Present");
+            }
+            else
+            {
+                Console.WriteLine("Absent");
+            }
+        }
+    }
+}
+
+
+
+
